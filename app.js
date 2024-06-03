61f83414cb2428efad4efc66e727d74051b1b326
Hamzaabro5 <hamzaabro683@gmail.com>
2024-06-03 09:29:14 +0500

    Ecommerce Functionallity using API

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ axios(`https://fakestoreapi.com/products`)
 
         <div class="card m-5" style="width: 24rem;">
         <div class="card-body p-5 border border-info border-2 ">
-        <img class="ms-5" src="${item.image}" width ="150" alt="">
+        <img class="ms-5" src="${item.image}" width ="200" height ="220" alt="">
         <h4 class="mt-5"> ${item.title}</h4><br/>
         <h5>Price: ${item.price}$</h5><br/>
         <p><b>Description</b>:<br/> ${item.description}</p>
