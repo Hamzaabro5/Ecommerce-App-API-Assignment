91e858b6ebad199f55a6d223ab51c7adb524f5ec
Hamzaabro5 <hamzaabro683@gmail.com>
2024-06-03 10:05:44 +0500

    Ecommerce Functionallity Using API

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const div = document.querySelector(`div`)
 
-axios(`https://fakestoreapi.com/products`)
-.then((res)=>{
-    console.log(res.data);
+
+
+
+
+// Rendering Data
+
+function renderData() {
+    axios(`https://fakestoreapi.com/products`)
+    .then((res)=>{
+        console.log(res.data);
     res.data.map((item)=>{
         div.innerHTML += `
         
         <hr/>
-
+        
         <div class="card m-5" style="width: 24rem;">
         <div class="card-body p-5 border border-info border-2 ">
         <img class="ms-5" src="${item.image}" width ="200" height ="220" alt="">
         <h4 class="mt-5"> ${item.title}</h4><br/>
         <h5>Price: ${item.price}$</h5><br/>
-        <p><b>Description</b>:<br/> ${item.description}</p>
+        <p><b>Description</b>:<br/> ${item.description}</p><br/>
+        <h6><i class="fa-regular fa-star"></i> Item Rating: ${item.rating.rate} </h6>
         </div>
         </div>
         `
     })
-
+    
 }).catch((err)=>{
     console.log(err);
 })
+}
+
+renderData()
+
+// Rendering Data
