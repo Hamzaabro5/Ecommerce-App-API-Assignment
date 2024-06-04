06d19871772a7f43c4489fae62d3fa55979e5bde
Hamzaabro5 <hamzaabro683@gmail.com>
2024-06-04 18:24:42 +0500

    Ecommerce Funtionallity Using API

--- app.js
+++ app.js
@@ -35,3 +35,49 @@ function renderData() {
 renderData()
 
 // Rendering Data
+
+// Filtering Data
+
+function filterBtn(btn) {
+    console.log(btn.innerHTML);
+    div.innerHTML = ``
+    
+    // const filtered = .filter(item => item.category === btn.innerHTML).map(item => {
+    //     div.innerHTML += `
+    //     <div class="card m-5" style="width: 24rem;">
+    //     <div class="card-body p-5 border border-info border-2 ">
+    //     <img class="ms-5" src="${item.image}" width ="200" height ="220" alt="">
+    //     <h4 class="mt-5"> ${item.title}</h4><br/>
+    //     <h5>Price: ${item.price}$</h5><br/>
+    //     <p><b>Description</b>:<br/> ${item.description}</p><br/>
+    //     <h6><i class="fa-regular fa-star"></i> Item Rating: ${item.rating.rate} </h6>
+    //     </div>
+    //     </div>
+    // `
+    // })
+
+    axios(`https://fakestoreapi.com/products`)
+    .then((res)=>{
+        console.log(res.data);
+        res.data.filter(item => item.category === btn.innerHTML).map((item)=>{
+            div.innerHTML += `
+            <hr/>
+            
+            <div class="card m-5" style="width: 24rem;">
+            <div class="card-body p-5 border border-info border-2 ">
+        <img class="ms-5" src="${item.image}" width ="200" height ="220" alt="">
+        <h4 class="mt-5"> ${item.title}</h4><br/>
+        <h5>Price: ${item.price}$</h5><br/>
+        <p><b>Description</b>:<br/> ${item.description}</p><br/>
+        <h6><i class="fa-regular fa-star"></i> Item Rating: ${item.rating.rate} </h6>
+        </div>
+        </div>
+        `
+    })
+    
+}).catch((err)=>{
+    console.log(err);
+})
+}
+
+// Filtering Data
